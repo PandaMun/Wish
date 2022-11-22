4fd81027fde7114d235402f1a157788eb2423325
김상현 <haan823@naver.com>
2022-11-22 15:33:08 +0900

    수정

--- giraffe-master/src/components/details/sidebar.vue
+++ giraffe-master/src/components/details/sidebar.vue
@@ -1,102 +1,75 @@
 <template>
   <div>
     <div>
-      <h3 class="text-h5 font-weight-medium pb-4">아파트 상세 정보</h3>
-
-      <v-divider></v-divider>
-
-      <div>
-        <v-row v-for="i in 5" :key="i" class="py-2">
-          <v-col cols="12" md="6" lg="5">
-            <v-card height="100%" flat>
-              <v-img
-                src="https://cdn.pixabay.com/photo/2021/01/22/16/55/camera-5940588_1280.jpg"
-                :aspect-ratio="16 / 9"
-                height="100%"
-              ></v-img>
-            </v-card>
-          </v-col>
-
-          <v-col>
-            <div>
-              <v-btn depressed color="accent" small>TRAVEL</v-btn>
-
-              <h3 class="text-h6 font-weight-bold primary--text py-3">
-                What do | need to know to start learning JavaScript?
-              </h3>
-
-              <div class="d-flex align-center">
-                <v-avatar color="accent" size="24">
-                  <v-icon dark small>mdi-feather</v-icon>
-                </v-avatar>
-
-                <div class="pl-2">Yan Lee · 03 Jan 2019</div>
-              </div>
-            </div>
-          </v-col>
-        </v-row>
-      </div>
-    </div>
-
-    <div class="pt-4">
-      <h3 class="text-h5 font-weight-medium pb-4">Category</h3>
-
-      <v-divider></v-divider>
-
-      <v-card color="accent" dark flat v-for="i in 5" :key="i" class="my-4">
-        <v-card-text class="d-flex justify-space-between align-center white--text">
-          <h6 class="text-h6">Travel</h6>
-
-          <div class="text-h6">47</div>
-        </v-card-text>
-      </v-card>
-    </div>
-
-    <div class="pt-4">
-      <h3 class="text-h5 font-weight-medium pb-4">Top Authors</h3>
-
-      <v-divider></v-divider>
-
-      <div class="pt-4">
-        <div class="d-flex align-center mb-6" v-for="i in 5" :key="i">
-          <v-avatar color="accent" size="64">
-            <v-icon dark x-large>mdi-feather</v-icon>
-          </v-avatar>
-
-          <div class="pl-2">
-            <div class="text-h6">Yan Lee</div>
-            <div class="text-subtitle-1">47 Articles</div>
-          </div>
+      <div
+        style="
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          padding-bottom: 10px;
+        "
+      >
+        <h4 class="text-h5 font-weight-bold pt-4 pb-4">아파트 상세 정보</h4>
+        <div>
+          <v-btn class="mx-2" fab small color="error">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
         </div>
       </div>
-    </div>
-
-    <div class="pt-4">
-      <h3 class="text-h5 font-weight-medium pb-4">Tags</h3>
-
       <v-divider></v-divider>
-
-      <v-row class="pt-4">
-        <v-col v-for="i in 7" :key="i" class="flex-shrink-0" cols="auto">
-          <v-chip color="accent">#Images</v-chip>
-        </v-col>
-      </v-row>
-    </div>
-
-    <div class="pt-4">
-      <h3 class="text-h5 font-weight-medium pb-4">Newsletter</h3>
-
-      <v-divider></v-divider>
-
-      <v-text-field
-        label="Your email adress"
-        solo
-        type="email"
-        outlined
-        flat
-        class="pt-4"
-      ></v-text-field>
-      <v-btn color="accent" block large>Subscrbe</v-btn>
+      <div>
+        <v-img src="../images/background.png" :aspect-ratio="16 / 9" height="100%"></v-img>
+        <h4 class="text-h6 font-weight-bold pt-4 pb-4">반포 자이 2차 아파트</h4>
+        <v-divider></v-divider>
+        <h4 class="text-h5 font-weight-bold pt-4 pb-4">거래내역</h4>
+        <template>
+          <v-data-table
+            font-size="1px"
+            :headers="headers"
+            :items="deals"
+            :items-per-page="5"
+            class="elevation-1"
+          ></v-data-table>
+        </template>
+      </div>
     </div>
   </div>
 </template>
+<script>
+import http from "@/util/http-common";
+export default {
+  data() {
+    return {
+      display: false,
+      aptCode: "11110000000001",
+      headers: [
+        { text: "계약일", value: "dealDate" },
+        { text: "매매가격(만원)", value: "dealAmount" },
+        { text: "층수(층)", value: "floor" },
+        { text: "면적(m2)", value: "area" },
+      ],
+      deals: [],
+      infos: [],
+    };
+  },
+  methods: {
+    makeDeals() {
+      let url = "/wish/apartments/info/" + this.aptCode;
+      http.get(url).then(({ data }) => {
+        console.log(data);
+        this.deals = data;
+      });
+    },
+    getInfo() {
+      let url = "/wish/apartments/info/" + this.aptCode;
+      http.get(url).then(({ data }) => {
+        console.log(data);
+        this.infos = data;
+      });
+    },
+  },
+  created() {
+    this.makeDeals();
+  },
+};
+</script>
--- giraffe-master/src/views/Map.vue
+++ giraffe-master/src/views/Map.vue
@@ -1,25 +1,83 @@
 <template>
   <div>
-    <div id="map" class="map"></div>
+    <v-row class="overflow-hidden">
+      <v-col class="pa-0 left-map" cols="9" lg="9" xl="9"> <div id="map" class="map"></div></v-col>
+      <v-col class="right-info overflow-auto pa-5" cols="3" lg="3" xl="3" style="height: 94vh">
+        <div>
+          <div>
+            <div
+              style="
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                padding-bottom: 10px;
+              "
+            >
+              <h4 class="text-h5 font-weight-bold pt-4 pb-4">아파트 상세 정보</h4>
+              <div>
+                <v-btn class="mx-2" fab small color="error">
+                  <v-icon>mdi-close</v-icon>
+                </v-btn>
+              </div>
+            </div>
+            <v-divider></v-divider>
+            <div>
+              <v-img src="../images/background.png" :aspect-ratio="16 / 9" height="100%"></v-img>
+              <h4 class="text-h6 font-weight-bold pt-4 pb-4">반포 자이 2차 아파트</h4>
+              <v-divider></v-divider>
+              <h4 class="text-h5 font-weight-bold pt-4 pb-4">거래내역</h4>
+              <template>
+                <v-data-table
+                  font-size="1px"
+                  :headers="headers"
+                  :items="deals"
+                  :items-per-page="5"
+                  class="elevation-1"
+                ></v-data-table>
+              </template>
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
+import http from "@/util/http-common";
 export default {
-  // props: {
-  //   name: {
-  //     type: String,
-  //     default: "",
-  //   },
-  //   age: {
-  //     type: Number,
-  //     default: 0,
-  //   },
-  // },
+  data() {
+    return {
+      display: false,
+      aptCode: "11110000000001",
+      headers: [
+        { text: "계약일", value: "dealDate" },
+        { text: "매매가격(만원)", value: "dealAmount" },
+        { text: "층수(층)", value: "floor" },
+        { text: "면적(m2)", value: "area" },
+      ],
+      deals: [],
+      infos: [],
+    };
+  },
   mounted() {
     window.kakao && window.kakao.maps ? this.initMap() : this.addKakaoMapScript();
   },
   methods: {
+    makeDeals() {
+      let url = "/wish/apartments/info/" + this.aptCode;
+      http.get(url).then(({ data }) => {
+        console.log(data);
+        this.deals = data;
+      });
+    },
+    getInfo() {
+      let url = "/wish/apartments/info/" + this.aptCode;
+      http.get(url).then(({ data }) => {
+        console.log(data);
+        this.infos = data;
+      });
+    },
     addKakaoMapScript() {
       const script = document.createElement("script");
       /* global kakao */
@@ -40,7 +98,11 @@ export default {
     },
   },
   created() {
+    this.makeDeals();
     console.log("map : " + this.$route.params);
+  },
+  components: {
+    // siderbar: () => import("@/components/details/sidebar"),
   },
 };
 </script>
@@ -48,6 +110,6 @@ export default {
 <style>
 .map {
   width: 100%;
-  height: 93vh;
+  height: 94vh;
 }
 </style>
