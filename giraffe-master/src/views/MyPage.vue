107e52a8222382a194460513fa816db777ce51f4
김상현 <haan823@naver.com>
2022-11-22 10:04:48 +0900

    my page

--- giraffe-master/src/views/MyPage.vue
+++ giraffe-master/src/views/MyPage.vue
@@ -1,243 +1,196 @@
 <template>
-  <div
-    class="pa-3 ma-4 mx-auto v-card v-sheet theme--light"
-    style="max-width: 500px"
-  >
-    <div class="v-card__title">
-      <hr
-        role="separator"
-        aria-orientation="horizontal"
-        class="my-1 v-divider theme--light"
-      />
-      <div class="mx-2">My Page</div>
-      <hr
-        role="separator"
-        aria-orientation="horizontal"
-        class="my-1 v-divider theme--light"
-      />
+  <div class="container col-5">
+    <div class="my-6">
+      <h2 class="text-h2 text-center font-weight-bold mt-5">My Page</h2>
     </div>
-    <div class="v-card__text">
-      <div
-        class="
-          v-input
-          theme--light
-          v-text-field
-          v-text-field--is-booted
-          v-text-field--enclosed
-          v-text-field--outlined
-        "
-      >
+    <br />
+    <form novalidate="novalidate" class="v-form">
+      <div class="v-input theme--light v-text-field v-text-field--is-booted">
         <div class="v-input__control">
           <div class="v-input__slot">
-            <fieldset aria-hidden="true">
-              <legend style="width: 0px">
-                <span class="notranslate">​</span>
-              </legend>
-            </fieldset>
+            <div class="v-text-field__slot">
+              <label
+                for="input-9"
+                class="v-label theme--light"
+                style="left: 0px; right: auto; position: absolute"
+                >Id</label
+              ><input
+                v-model="id"
+                style="padding-left: 100px"
+                required="required"
+                id="input-9"
+                type="text"
+                readonly
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="v-input theme--light v-text-field v-text-field--is-booted">
+        <div class="v-input__control">
+          <div class="v-input__slot">
+            <div class="v-text-field__slot">
+              <label
+                for="input-9"
+                class="v-label theme--light"
+                style="left: 0px; right: auto; position: absolute"
+                >Name</label
+              ><input
+                v-model="name"
+                required="required"
+                style="padding-left: 100px"
+                id="input-9"
+                type="text"
+                readonly
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="v-input theme--light v-text-field v-text-field--is-booted">
+        <div class="v-input__control">
+          <div class="v-input__slot">
+            <div class="v-text-field__slot">
+              <label
+                for="input-9"
+                class="v-label theme--light"
+                style="left: 0px; right: auto; position: absolute"
+                >Address</label
+              ><input
+                v-model="address"
+                required="required"
+                style="padding-left: 100px"
+                id="input-9"
+                type="text"
+                readonly
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="v-input theme--light v-text-field v-text-field--is-booted">
+        <div class="v-input__control">
+          <div class="v-input__slot">
             <div class="v-text-field__slot">
               <label
                 for="input-12"
                 class="v-label theme--light"
                 style="left: 0px; right: auto; position: absolute"
-                >User Id</label
-              ><input id="input-12" type="text" />
-            </div>
-          </div>
-          <div class="v-text-field__details">
-            <div class="v-messages theme--light">
-              <div class="v-messages__wrapper"></div>
+                >E-mail</label
+              ><input
+                v-model="email"
+                required="required"
+                style="padding-left: 100px"
+                id="input-12"
+                type="text"
+                readonly
+              />
             </div>
           </div>
         </div>
       </div>
-      <div
-        class="
-          v-input
-          theme--light
-          v-text-field
-          v-text-field--is-booted
-          v-text-field--enclosed
-          v-text-field--outlined
-        "
-      >
+      <div class="v-input theme--light v-text-field v-text-field--is-booted">
         <div class="v-input__control">
           <div class="v-input__slot">
-            <fieldset aria-hidden="true">
-              <legend style="width: 0px">
-                <span class="notranslate">​</span>
-              </legend>
-            </fieldset>
             <div class="v-text-field__slot">
               <label
-                for="input-15"
+                for="input-9"
                 class="v-label theme--light"
                 style="left: 0px; right: auto; position: absolute"
-                >Last Name</label
-              ><input id="input-15" type="text" />
-            </div>
-          </div>
-          <div class="v-text-field__details">
-            <div class="v-messages theme--light">
-              <div class="v-messages__wrapper"></div>
+                >Phone</label
+              ><input
+                v-model="phone"
+                required="required"
+                style="padding-left: 100px"
+                id="input-9"
+                type="text"
+                readonly
+              />
             </div>
           </div>
         </div>
       </div>
-      <div
-        class="
-          v-input
-          theme--light
-          v-text-field
-          v-text-field--is-booted
-          v-text-field--enclosed
-          v-text-field--outlined
-        "
-      >
-        <div class="v-input__control">
-          <div class="v-input__slot">
-            <fieldset aria-hidden="true">
-              <legend style="width: 0px">
-                <span class="notranslate">​</span>
-              </legend>
-            </fieldset>
-            <div class="v-text-field__slot">
-              <label
-                for="input-18"
-                class="v-label theme--light"
-                style="left: 0px; right: auto; position: absolute"
-                >Email</label
-              ><input id="input-18" type="text" />
-            </div>
-          </div>
-          <div class="v-text-field__details">
-            <div class="v-messages theme--light">
-              <div class="v-messages__wrapper"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div
-        class="
-          v-input
-          theme--light
-          v-text-field
-          v-text-field--is-booted
-          v-text-field--enclosed
-          v-text-field--outlined
-        "
-      >
-        <div class="v-input__control">
-          <div class="v-input__slot">
-            <fieldset aria-hidden="true">
-              <legend style="width: 0px">
-                <span class="notranslate">​</span>
-              </legend>
-            </fieldset>
-            <div class="v-text-field__slot">
-              <label
-                for="input-21"
-                class="v-label theme--light"
-                style="left: 0px; right: auto; position: absolute"
-                >Password</label
-              ><input id="input-21" type="text" />
-            </div>
-          </div>
-          <div class="v-text-field__details">
-            <div class="v-messages theme--light">
-              <div class="v-messages__wrapper"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <button
-        type="button"
-        class="
-          success
-          v-btn v-btn--block v-btn--is-elevated v-btn--has-bg
-          theme--light
-          v-size--x-large
-        "
-      >
-        <span class="v-btn__content">Create Account</span>
-      </button>
-      <div class="d-flex my-3">
-        <hr
-          role="separator"
-          aria-orientation="horizontal"
-          class="my-1 v-divider theme--light"
-        />
-        <div class="text-overline mx-1">Or Sign In With</div>
-        <hr
-          role="separator"
-          aria-orientation="horizontal"
-          class="my-1 v-divider theme--light"
-        />
-      </div>
-      <div class="d-flex justify-space-between">
+
+      <br />
+      <div style="display: flex; justify-content: center">
+        <!-- <button
+          type="button"
+          class="mr-4 v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default success"
+        >
+          <span class="v-btn__content"> 확인 </span>
+        </button> -->
         <button
           type="button"
-          class="
-            flex-grow-1
-            v-btn v-btn--outlined
-            theme--light
-            v-size--large
-            secondary--text
-            text--lighten-2
-          "
+          class="mr-4 v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default error"
+          @click="dialog = true"
         >
-          <span class="v-btn__content"
-            ><i
-              aria-hidden="true"
-              class="v-icon notranslate mdi mdi-facebook theme--light"
-            ></i
-          ></span></button
-        ><button
-          type="button"
-          class="
-            flex-grow-1
-            mx-2
-            v-btn v-btn--outlined
-            theme--light
-            v-size--large
-            secondary--text
-            text--lighten-2
-          "
-        >
-          <span class="v-btn__content"
-            ><i
-              aria-hidden="true"
-              class="v-icon notranslate mdi mdi-twitter theme--light"
-            ></i
-          ></span></button
-        ><button
-          type="button"
-          class="
-            flex-grow-1
-            v-btn v-btn--outlined
-            theme--light
-            v-size--large
-            secondary--text
-            text--lighten-2
-          "
-        >
-          <span class="v-btn__content"
-            ><i
-              aria-hidden="true"
-              class="v-icon notranslate mdi mdi-google theme--light"
-            ></i
-          ></span>
+          <span class="v-btn__content"> 비밀번호 변경 </span>
         </button>
       </div>
-      <div class="text-overline text-uppercase mt-3 text-center">
-        <div class="v-responsive mx-auto" style="max-width: 280px">
-          <div class="v-responsive__content">
-            By signing up, you agree to the
-            <a href="#">Terms of Service</a> &amp;
-            <a href="#">Privacy Policy</a>
-          </div>
-        </div>
-      </div>
-    </div>
+    </form>
+    <v-dialog persistent v-model="dialog" max-width="500px" :retain-focus="false">
+      <v-card>
+        <v-card-title>
+          <span class="text-h5 font-weight-bold">새 비밀번호 입력</span>
+        </v-card-title>
+        <v-card-text class="pb-0">
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field v-model="password" required></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            class="font-weight-bold"
+            text
+            @click="modifyPassword('ssafy')"
+          >
+            변경
+          </v-btn>
+          <v-btn color="blue darken-1" class="font-weight-bold" text @click="dialog = false">
+            취소
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
-  
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+
+      id: "ssafy",
+      name: "김싸피",
+      address: "장덕동",
+      email: "ssafy@ssafy.com",
+      phone: "010-1234-5678",
+      password: "admin",
+    };
+  },
+  methods: {
+    getUserInfo(id) {
+      let url = "wish/users/" + id;
+      http.get(url).then(({ data }) => {
+        this.id = data.id;
+        this.name = data.name;
+        this.address = data.address;
+        this.email = data.email;
+        this.phone = data.phoneNumber;
+      });
+    },
+    modifyPassword(id) {
+      let url = "wish/users/" + id;
+      http.put(url, { password: this.password }).then(({ data }) => {
+        console.log(data);
+      });
+    },
+  },
+};
+</script>
